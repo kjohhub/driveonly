51db3f7c48a9f2e5ce6a51d6aa08621eabea4567
kjoh <rudwls@gmail.com>
2021-05-12 17:48:12 +0900

    add detect_face.py (mobillenet ssd)

---
+++ README.md
@@ -0,0 +1,4 @@
+## video2image : 동영상을 이미지파일들로 변경
+- ex) python video2image /input/test.mp4 /images/
+## detect_face : 동영상에서 얼굴검출하여 동영상으로 저장
+- ex) python detect_face /input/test.mp4 /output/test.mp4
---
+++ video2image.py
@@ -0,0 +1,20 @@
+import cv2
+import sys
+
+if len(sys.argv) < 3:
+    sys.exit()
+
+src = sys.argv[1]
+dst = sys.argv[2]
+cap = cv2.VideoCapture(src)
+idx = 0
+
+while True:
+    ret, image = cap.read()
+    if ret == 0:
+        break
+    else:
+        fname = "{0}{1:05d}.jpg".format(dst, idx)
+        cv2.imwrite(fname, image)
+        idx += 1
+

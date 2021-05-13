6e99f657327eec7306cc3c0f7c952a5f2e869132
kjohhub <rudwls@gmail.com>
2021-05-13 17:33:36 +0900

    modify crop_face.py

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 ## video2image : 동영상을 이미지파일들로 변경
 - ex) python video2image /input/test.mp4 /images/
+
 ## detect_face : 동영상에서 얼굴검출하여 동영상으로 저장
 - ex) python detect_face /input/test.mp4 /output/test.mp4
+
+## crop_face : 동영상에서 얼굴 crop하여 이미지파일로 저장
+- ex)python crop_face /input/test.mp4 /output/test_crop/
--- crop_face.py
+++ crop_face.py
@@ -27,12 +27,7 @@ NUM_CLASSES = 2
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
-'''
-def load_image_into_numpy_array(image):
-  (im_width, im_height) = image.size
-  return np.array(image.getdata()).reshape(
-      (im_height, im_width, 3)).astype(np.uint8)
-'''
+
 if len(sys.argv) < 3:
     sys.exit()
 
@@ -40,8 +35,9 @@ src = sys.argv[1]
 dst = sys.argv[2]
 cap = cv2.VideoCapture(src)
 
-
-os.mkdir(dst)
+# create directory if not exist
+if os.path.isdir(dst) == False:
+    os.mkdir(dst)
 
 detection_graph = tf.Graph()
 with detection_graph.as_default():
@@ -88,20 +84,19 @@ with detection_graph.as_default():
       #print(scores.shape,scores)
       #print(classes.shape,classes)
       #print(num_detections)
-      # Visualization of the results of a detection.
-      vis_util.visualize_boxes_and_labels_on_image_array(
-#          image_np,
-          image,
-          np.squeeze(boxes),
-          np.squeeze(classes).astype(np.int32),
-          np.squeeze(scores),
-          category_index,
-          use_normalized_coordinates=True,
-          line_thickness=4)
 
-      fname = "{0}{1:06d}.jpg".format(dst, index)
-      cv2.imwrite(fname, image)
-      index += 1
+      # crop
+      boxes = np.squeeze(boxes)
+      if len(boxes) > 0:
+        h, w, c = image.shape
+        y_min = int(boxes[0, 0] * h)
+        x_min = int(boxes[0, 1] * w)
+        y_max = int(boxes[0, 2] * h)
+        x_max = int(boxes[0, 3] * w)
+        crop_image = image[y_min:y_max, x_min:x_max].copy()
+
+        fname = "{0}{1:06d}.jpg".format(dst, index)
+        cv2.imwrite(fname, crop_image)
+        index += 1
 
     cap.release()
-    out.release()
